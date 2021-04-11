35b9abab098c6350d1807689ae42f7c908d13fdd
K Truong <truong.k77@gmail.com>
2021-04-11 00:39:15 -0700

    Fix error in changes

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require("dotenv").config();
 const http = require("http");
 const https = require("https");
-const app = require("express")();
+const app = require("./modules/app.js");
 const redirectApp = require("./modules/redirectapp.js");
 const credentials = require("./modules/ssl");
+const dbconnection = require("./modules/database.js");
 
 /**
  * Server Configuration
  */
 const httpPort = process.env.HTTP_PORT || 80;
 const httpsPort = process.env.HTTPS_PORT || 443;
-
-app.use(express.static(path.join(__dirname, "web-build")));
-
-app.get("*", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "/web-build/index.html"));
-});
 
 /**
  * Server Initialization
@@ -28,6 +23,7 @@ if (process.env.NODE_ENV == "production") {
         httpsServer.listen(httpsPort);
     } catch (error) {
         console.log(error.message);
+        dbconnection.end();
     }
 } else {
     try {
@@ -35,5 +31,6 @@ if (process.env.NODE_ENV == "production") {
         httpServer.listen(httpPort);
     } catch (error) {
         console.log(error.message);
+        dbconnection.end();
     }
 }
