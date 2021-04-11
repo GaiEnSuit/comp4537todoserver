166b06341ff959e28961d85d402ed51c1cc4590c
K Truong <truong.k77@gmail.com>
2021-04-11 00:19:57 -0700

    remove client server logic completeley

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const http = require("http");
 const https = require("https");
-const app = require("./modules/app.js");
+const app = require("express")();
 const redirectApp = require("./modules/redirectapp.js");
 const credentials = require("./modules/ssl");
-const dbconnection = require("./modules/database.js");
 
 /**
  * Server Configuration
  */
 const httpPort = process.env.HTTP_PORT || 80;
 const httpsPort = process.env.HTTPS_PORT || 443;
+
+app.use(express.static(path.join(__dirname, "web-build")));
+
+app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/web-build/index.html"));
+});
 
 /**
  * Server Initialization
@@ -23,7 +28,6 @@ if (process.env.NODE_ENV == "production") {
         httpsServer.listen(httpsPort);
     } catch (error) {
         console.log(error.message);
-        dbconnection.end();
     }
 } else {
     try {
@@ -31,6 +35,5 @@ if (process.env.NODE_ENV == "production") {
         httpServer.listen(httpPort);
     } catch (error) {
         console.log(error.message);
-        dbconnection.end();
     }
 }
